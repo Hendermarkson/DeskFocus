0ebaa73fbb092758cbd9e2ad5a4618b4c8299d54
Mark Henderson <mark-henderson@hotmail.com>
2021-02-03 21:05:44 +0100

    Implemented AppDrawer consume state

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,28 +1,34 @@
+import 'package:desk_focus/models/settings_data.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../routes.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          DrawerHeader(
-            child: Text('Desk Focus'),
-          ),
-          DrawerItem(
-            icon: Icons.check_box,
-            title: 'Tasks',
-            route: Routes.tasks,
-          ),
-          DrawerItem(
-            icon: Icons.settings,
-            title: 'Settings',
-            route: Routes.settings,
-          )
-        ],
+    return Consumer<SettingsData>(
+      builder: (_, settings, child) => Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              child: Text('Desk Focus'),
+            ),
+            DrawerItem(
+              icon: Icons.check_box,
+              title: 'Tasks',
+              route: Routes.tasks,
+              settings: settings,
+            ),
+            DrawerItem(
+              icon: Icons.settings,
+              title: 'Settings',
+              route: Routes.settings,
+              settings: settings,
+            )
+          ],
+        ),
       ),
     );
   }
@@ -32,18 +38,25 @@ class DrawerItem extends StatelessWidget {
   final IconData icon;
   final String title;
   final String route;
-
-  const DrawerItem({Key key, this.icon, this.title, this.route})
-      : super(key: key);
+  final SettingsData settings;
+  const DrawerItem({
+    Key key,
+    this.icon,
+    this.title,
+    this.route,
+    this.settings,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      selected: settings.selectedPage == route,
       leading: Icon(this.icon),
       title: Text(this.title),
       onTap: () {
         Navigator.pop(context);
-        Navigator.pushReplacementNamed(context, this.route);
+        Navigator.pushReplacementNamed(context, route);
+        settings.setSelectedPage(route);
       },
     );
   }
